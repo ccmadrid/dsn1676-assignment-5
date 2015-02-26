0d2331f0a94c2b11fa63f6273bf211594df192bf
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-02-26 13:16:56 -0500

    added some js functionality & styles
    able to add lis to list

--- js/main.js
+++ js/main.js
@@ -8,5 +8,13 @@ var $items = $('.list li');
 $form.on('submit', function (e) {
 	e.preventDefault();
 
+	var $li = $('<li class="js-li">');
+	var inputValue = $input.val();
+	var $inputA = $('<a href="#" class="js-item">').html($input.val())
+	var $close = '<a href="#" class="js-close">X</a>'
+
+	$li.append($inputA).append($close);
+	$list.append($li);
+	console.log($close);
 
 });
