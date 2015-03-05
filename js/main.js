8bbec8f8610adf42c066d657b5c485f3dc0d8e42
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-03-05 09:44:31 -0500

    changed structure of variables

--- js/main.js
+++ js/main.js
@@ -2,19 +2,13 @@ var $form = $('.form');
 var $input = $('.input');
 var $list = $('.list');
 
-var $items = $('.list li');
-var $close = $('.js-close')
-
 $form.on('submit', function (e) {
 	e.preventDefault();
 
-	var $li = $('<li class="js-li">');
+	var $li = $('<li>').html($name.val());
 	var inputValue = $input.val();
-	var $inputA = $('<a href="#" class="js-item">').html($input.val())
-	var $close = '<a href="#" class="js-close">X</a>'
+	var $close = $('<button class="close">X</button>')
 
-	$li.append($inputA).append($close);
-	$list.append($li);
 });
 
 $list.on('click', 'li', function (e) {
@@ -22,3 +16,5 @@ $list.on('click', 'li', function (e) {
 	console.log($list);
 
 });
+
+
